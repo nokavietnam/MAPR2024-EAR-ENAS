84ca2bbbf16daf1cb9b3c0af51373616909e1217
noka <noka.hieunt@gmail.com>
2024-03-22 23:23:48 +0700

    add noise cifar100 and imagenet

--- convert_data_benmark.py
+++ convert_data_benmark.py
@@ -10,11 +10,14 @@ list_ops = np.array(['none', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'av
 # {'nb201-string': '|avg_pool_3x3~0|+|nor_conv_1x1~0|skip_connect~1|+|nor_conv_1x1~0|skip_connect~1|skip_connect~2|', 'isomorph': '0'} 
 # to 
 # 421211
-def decode_arch(arch):
+
+
+def decode_arch(arch) -> str:
     return ''.join(
         str(np.nonzero(list_ops == item.split('~')[0])[0][0]) for item in filter(None, arch.split('|')) if item != '+')
 
-def main(noise, dataset):
+
+def main(noise, dataset) -> None:
     data_pdg = json.load(open(f'data/robustness-data/{dataset}/{noise}@Linf_accuracy.json', 'r'))
     data_pdg_acc = data_pdg[f'{dataset}'][f'{noise}@Linf']['accuracy']
     meta_data = json.load(open('data/robustness-data/meta.json', 'r'))
@@ -35,8 +38,8 @@ def main(noise, dataset):
         print(f'{item} - {data_result[item]}')
         break
 
-    #p.dump(data_result, open(f'data/robustness/result/data_{noise}_acc.p', 'wb'))
+    p.dump(data_result, open(f'data/robustness-data/result/data_{noise}_{dataset}_acc.p', 'wb'))
 
 
 if __name__ == '__main__':
-    main(noise='aa_apgd-ce', dataset='cifar10')
+    main(noise='fgsm', dataset='cifar10')
